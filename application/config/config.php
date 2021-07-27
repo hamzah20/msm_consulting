25e199c1ef53afd1e201f47d5969f5a5e8c83a54
Hamzah Aji Pratama <hamzah20@users.noreply.github.com>
2021-07-27 16:46:56 +0700

    Bisa ngelempar ke login

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://localhost/msm_consulting/';
+$config['base_url'] = 'http://localhost/msm_consulting/';
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,9 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Dashboard extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('CMS/index');
+		$this->load->view('CMS/dashbord');
 	}
 }
