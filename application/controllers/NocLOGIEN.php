ebc6d6c5cfd4d76d85b021c9094efec7893aa755
Hamzah Aji Pratama <hamzah20@users.noreply.github.com>
2021-08-02 13:32:37 +0700

    biar ga error diawal

--- application/controllers/NocLOGIEN.php
+++ application/controllers/NocLOGIEN.php
@@ -5,7 +5,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class NocLOGIEN extends CI_Controller
 {
 
-    public function __construct()
+    public function __construct()  
     {
         parent::__construct();
 
--- system/libraries/Session/drivers/Session_files_driver.php
+++ system/libraries/Session/drivers/Session_files_driver.php
@@ -113,7 +113,7 @@ class CI_Session_files_driver extends CI_Session_driver implements SessionHandle
 			$this->_config['save_path'] = rtrim(ini_get('session.save_path'), '/\\');
 		}
 
-		$this->_sid_regexp = $this->_config['_sid_regexp'];
+		$this->_sid_regexp = $this->_config['_sid_regexp'];  
 
 		isset(self::$func_overload) OR self::$func_overload = (extension_loaded('mbstring') && ini_get('mbstring.func_overload'));
 	}
